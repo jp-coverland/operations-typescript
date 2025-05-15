74486674cebd826262d84645682d139b292612ac
jp-coverland <jimin.coverland@gmail.com>
2025-05-15 10:18:15 -0700

    Remove secrets from client side.

--- linnworks/authorize.ts
+++ linnworks/authorize.ts
@@ -1,8 +1,13 @@
-import { linnworksAuthPayload } from "../constants/constants";
 import dotenv from "dotenv";
 import axios from "axios";
 
 dotenv.config();
+
+const linnworksAuthPayload = {
+  ApplicationId: process.env.LINNWORKS_APPLICATION_ID,
+  ApplicationSecret: process.env.LINNWORKS_APPLICATION_SECRET,
+  Token: process.env.LINNWORKS_API_TOKEN,
+};
 
 export async function getAuthToken(): Promise<any> {
   try {
