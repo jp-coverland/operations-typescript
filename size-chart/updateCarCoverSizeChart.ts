ee5d71a8d9c57f883459c02f510c93790aeb5d7f
jp-coverland <jimin.coverland@gmail.com>
2025-05-21 11:08:06 -0700

    Make combined the main update function.

--- size-chart/updateCarCoverSizeChart.ts
+++ size-chart/updateCarCoverSizeChart.ts
@@ -5,7 +5,7 @@ import { createContextLogger } from "../constants/logger";
 import { authorize } from "../google-sheets/authClient";
 
 async function getCarCoverSizeChart() {
-  const { data, error } = await supabaseCoverlandSizeChart.rpc("get_ordered_car_covers");
+  const { data, error } = await supabaseCoverlandSizeChart.rpc("get_grouped");
 
   if (error) {
     throw new Error(`Supabase query failed: ${JSON.stringify(error, null, 2)}`);
@@ -21,8 +21,10 @@ async function updateCarCoverSizeChart(auth: any) {
   const timestamp = DateTime.now().setZone("America/Los_Angeles").toFormat("yyyy-MM-dd HH:mm:ss");
   const SHEETS_ID = "13tu-KiJFgiz0dD5GUPB6-AR6nh8BdD2MCNMem9aClys";
 
-  const carCoverData = await getCarCoverSizeChart();
-  const carCoverPayload = (carCoverData.data as any[]).map(
+  const groupedData = await getCarCoverSizeChart();
+  let lastGroupedFNumber: any = null;
+
+  const groupedPayload1 = (groupedData.data as any[]).map(
     ({
       f_number,
       vehicle_type,
@@ -34,18 +36,31 @@ async function updateCarCoverSizeChart(auth: any) {
       submodel3,
       submodel4,
       concatenated,
+      notes,
       size,
       custom_size,
       vehicle_length,
-      notes,
-    }) => [f_number, vehicle_type, year_generation, make, model, submodel1, submodel2, submodel3, submodel4, concatenated, size, custom_size, vehicle_length, notes]
+    }) => [f_number, vehicle_type, year_generation, make, model, submodel1, submodel2, submodel3, submodel4, concatenated, notes, size, custom_size, vehicle_length]
   );
 
+  const groupedPayload2 = (groupedData.data as any[]).map(({ grouped_f_number, grouped_info, grouped_size, grouped_custom_size, grouped_notes }) => {
+    const isFirstInGroup = grouped_f_number !== lastGroupedFNumber;
+    lastGroupedFNumber = grouped_f_number;
+
+    return [
+      isFirstInGroup ? grouped_f_number : "",
+      isFirstInGroup ? grouped_info : "",
+      isFirstInGroup ? grouped_size : "",
+      isFirstInGroup ? grouped_custom_size : "",
+      isFirstInGroup ? grouped_notes : "",
+    ];
+  });
+
   try {
-    // car cover
+    // car covers
     await sheets.spreadsheets.values.update({
       spreadsheetId: SHEETS_ID,
-      range: "car_covers!O1",
+      range: "car_cover_size_chart!U1",
       valueInputOption: "RAW",
       requestBody: {
         values: [[`Last updated: ${timestamp}`]],
@@ -53,14 +68,22 @@ async function updateCarCoverSizeChart(auth: any) {
     });
     await sheets.spreadsheets.values.clear({
       spreadsheetId: SHEETS_ID,
-      range: "car_covers!A2:N",
+      range: "car_cover_size_chart!A2:T",
     });
     await sheets.spreadsheets.values.update({
       spreadsheetId: SHEETS_ID,
-      range: "car_covers!A2",
+      range: "car_cover_size_chart!A2",
       valueInputOption: "RAW",
       requestBody: {
-        values: carCoverPayload,
+        values: groupedPayload1,
+      },
+    });
+    await sheets.spreadsheets.values.update({
+      spreadsheetId: SHEETS_ID,
+      range: "car_cover_size_chart!P2",
+      valueInputOption: "RAW",
+      requestBody: {
+        values: groupedPayload2,
       },
     });
 
@@ -69,5 +92,4 @@ async function updateCarCoverSizeChart(auth: any) {
     logger.error(`[error] Failed to update car cover size chart: ${error}`);
   }
 }
-
 authorize().then(updateCarCoverSizeChart).catch(console.error);
